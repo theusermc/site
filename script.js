d53b2412f017dd75dc055debfb7bc85265bace73
Glitch (rintyuu) <none>
2021-06-17 23:40:50 +0000

    🐿🌺 Checkpoint ./rntu-redirect.html:8923719/1615 ./script.js:8923719/1712

--- script.js
+++ script.js
@@ -4,7 +4,14 @@
 // prints "hi" in the browser's dev tools console
 console.log("hi");
 
-
 function rintyuudownload() {
-  location.replace("https://cdn.glitch.com/2ff3a645-91cc-42fd-bae2-3ba8fb0148a0%2Frintyuu-theme-unzip-me.zip?v=1623972051939")
+  location.replace(
+    "https://cdn.glitch.com/2ff3a645-91cc-42fd-bae2-3ba8fb0148a0%2Frintyuu-theme-unzip-me.zip?v=1623972051939"
+  );
+}
+
+function rintyuualert() {
+  alert(
+    "redirected for security reasons. go ahead and continue."
+  );
 }
