26951ed25703b2cbe38c72feeba5942e304490f7
Glitch (rintyuu) <none>
2021-06-18 17:35:47 +0000

    Rewound to commit cf84e12f90e30719eb4af562f5a04ff9dae74273 + 0 edits

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-function rinbddw() {
+function rintyuudownload() {
   location.replace(
     "https://cdn.glitch.com/2ff3a645-91cc-42fd-bae2-3ba8fb0148a0%2Frintyuu-theme-unzip-me.zip?v=1623972051939"
   );
@@ -6,10 +6,4 @@ function rinbddw() {
 
 function rintyuualert() {
   alert("redirected for security reasons. go ahead and continue.");
-}
-
-funtion rinctdw() {
-  location.replace(
-  "https://cdn.glitch.com/2ff3a645-91cc-42fd-bae2-3ba8fb0148a0%2Frintyuu%20theme.zip?v=1624036998094"
-  );
 }
