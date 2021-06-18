4dff912f02cad79265a9fc50e74baad526dbdd85
Glitch (rintyuu) <none>
2021-06-18 00:15:08 +0000

    😅🍜 Checkpoint ./index.html:8923719/3647 ./script.js:8923719/161 ./assets/script.js:8923719/272 ./rintyuus/gallery.html:8923719/38 ./rintyuus/downloads.html:8923719/116 ./rintyuus/bot.html:8923719/120 ./rintyuus/success.html:8923719/3757

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,3 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
-
-// prints "hi" in the browser's dev tools console
-console.log("hi");
-
 function rintyuudownload() {
   location.replace(
     "https://cdn.glitch.com/2ff3a645-91cc-42fd-bae2-3ba8fb0148a0%2Frintyuu-theme-unzip-me.zip?v=1623972051939"
