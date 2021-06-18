d6b1d00712f1a04c4e98f351202922fca3b632f1
Glitch (rintyuu) <none>
2021-06-18 17:45:46 +0000

    🌠🏵 Checkpoint ./assets/script.js:8923719/2 ./rintyuus/downloads.html:8923719/4

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-function rintyuudownload() {
+function rintyuubddownload() {
   location.replace(
     "https://cdn.glitch.com/2ff3a645-91cc-42fd-bae2-3ba8fb0148a0%2Frintyuu-theme-unzip-me.zip?v=1623972051939"
   );
