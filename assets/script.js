0c6e373a19c6bd2777bf1d5808c885be02c4ebe5
Glitch (rintyuu) <none>
2021-07-06 01:59:14 +0000

    🏹👜 Checkpoint ./assets/script.js:8923719/454 ./rintyuus/downloads.html:8923719/5887 ./rintyuus/bot.html:8923719/3069 ./rintyuus/about.html:8923719/2913 ./index.html:8923719/30

--- assets/script.js
+++ assets/script.js
@@ -4,16 +4,6 @@ function rintyuubddownload() {
   );
 }
 
-function rintyuualert() {
-  alert("redirected for security reasons. go ahead and continue.");
-}
-
-function rintyuuctdownload() {
-  location.replace(
-    "https://cdn.glitch.com/2ff3a645-91cc-42fd-bae2-3ba8fb0148a0%2Frintyuu%20theme.zip?v=1624041490229"
-  );
-}
-
 setTimeout(rintyuu() {
   location.replace("https://rintyuu.glitch.me/");
 }, 2000);
