9bf72ea0f19ebbbcd807e53ed1ba9c1151cb3b97
Glitch (rintyuu) <none>
2021-08-16 18:18:42 +0000

    🍏🐔 Checkpoint ./assets/script.js:8923719/1197 ./rintyuus/testing.html:8923719/5143

--- assets/script.js
+++ assets/script.js
@@ -15,20 +15,3 @@ funtion alertcontact() {
 function myFunction() {
   location.replace("https://www.w3schools.com")
 }
-
-var x = document.getElementById("coord");
-
-      function getLocation() {
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(showPosition);
-        } else {
-          x.innerHTML = "Geolocation is not supported by this browser.";
-        }
-      }
-
-      function showPosition(position) {
-        x.innerHTML =
-          " " +
-          position.coords.latitude +
-          ", " +
-          position.coords.longitude;}
