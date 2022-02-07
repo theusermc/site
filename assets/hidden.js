ca66fa950e8ed503dfbc8f5ca0c4b10fce26d62b
Glitch (rintyuu) <none>
2022-02-07 02:06:09 +0000

    👾⚡️ Checkpoint

--- assets/hidden.js
+++ assets/hidden.js
@@ -1,3 +1,4 @@
 function hiddenSite() {
-  document.getElementById("hidden-site").innerHTML = '<p>behold! the way to the hidden website opened up! click</p><a href="misc.html">here, to open it</a>';
+  document.getElementById("hidden-site").innerHTML =
+    '<p>behold! the way to the hidden website opened up! click</p><a href="misc.html">here, to open it</a>';
 }
