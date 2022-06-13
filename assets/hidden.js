e5aee5599f9780b88f964f0ead955179e61cd342
Glitch (rintyuu) <none>
2022-06-13 18:47:27 +0000

    🍰🏀 Checkpoint ./assets/hidden.js:8923719/81 ./index.html:8923719/1810 ./rintyuus/downloads.html:8923719/9138

--- assets/hidden.js
+++ assets/hidden.js
@@ -1,3 +1,3 @@
 function hiddenSite() {
-  document.getElementById("hidden-site").innerHTML = '<p>behold! the gate to the hidden website opened up! click</p><a href="misc.html">here, to open it</a>';
+  document.getElementById("hidden-site").innerHTML = '<p>behold! the gate to the hidden website has opened! click</p><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">here, to open it</a>';
 }
